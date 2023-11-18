cc9182045e4e4d6e61af65680bbca5a71d173748
Sandeep Tomar <tomarsandeep199192@gmail.com>
2023-11-18 17:10:20 +0530

    Initial Commit

---
+++ CreditCard/CreditCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCardApp.swift
+//  CreditCard
+//
+//  Created by Sandeep.Tomar on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct CreditCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
